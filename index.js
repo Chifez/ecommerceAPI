b3e9b4effbc363ffb16214a500a872a18dbad87a
emmanuel <chifez1@gmail.com>
2023-12-22 21:50:06 +0000

    working on the api for authentication

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose');
 const app = express();
 const dotenv = require('dotenv');
 const userRoute = require('./routes/users');
-
+const authRoute = require('./routes/auth');
 dotenv.config();
 app.use(express.json());
 
 app.use('/api/user', userRoute);
+app.use('/api/auth', authRoute);
 mongoose
   .connect(process.env.MONGODB_SECRET_KEY)
   .then(() => console.log('connected sucessfully'))
